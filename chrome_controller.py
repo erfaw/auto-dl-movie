b0681a4a536c656c95bd8d55b60582f3d017d478
erfaw <erfawn.h@gmail.com>
2026-07-01 00:15:25 +0330

    docs: docstring for __init__(), start(), close()
    -start familiarize myself with Google-Style Docstrings.

--- chrome_controller.py
+++ chrome_controller.py
@@ -8,9 +8,23 @@ class ChromeController:
     """
 
     def __init__(self, chrome_path: Path) -> None:
+        """
+        A Chrome based controller powered by `Playwright` based on a main page to explore and automize some basic works.
+
+        Args:
+            chrome_path (:obj:`Path`): Path to `chrome.exe` file on system .
+        
+        Returns:
+            None
+        """
         self.chrome_path = chrome_path
 
     def start(self):
+        """
+        set `pw`, run `start()` and make a `self.main_page` to work on it.
+
+        actually starts a playwright loop to be able make browser.
+        """
         self.pw: Playwright = sync_playwright().start()
         self.browser: Browser = self.pw.chromium.launch(
             executable_path=self.chrome_path,
@@ -19,6 +33,9 @@ class ChromeController:
         self.main_page: Page = self.browser.new_page()
 
     def close(self):
+        """
+        run `stop()` on `self.pw`.
+        """
         self.pw.stop()
 
     def make_page(self):
