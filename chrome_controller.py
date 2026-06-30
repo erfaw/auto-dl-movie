76d6f19788c30cc935144e64524463297c682788
erfaw <erfawn.h@gmail.com>
2026-07-01 02:09:36 +0330

    feat: scrape and print 5 first movies details
    -declare get_movies_list() for ChromeController()
    -write docstring for it.
    -scrape details about name, year and directors and return,
    -in main call this method and print it.

--- chrome_controller.py
+++ chrome_controller.py
@@ -38,5 +38,39 @@ class ChromeController:
         """
         self.pw.stop()
 
-    def get_movies_list(self):
-        pass
+    def get_movies_list(self, ul_selector):
+        """
+        Scrape all movies and slice 5 first then return.
+
+        Args:
+            ul_selector: XPath or css selector of some element in DOM.
+
+        Returns:
+            list:
+        """
+        ul_continer = self.main_page.locator(ul_selector)
+        ul_continer.wait_for()
+        li_first_child_selector = "> li"
+        movies = ul_continer.locator(li_first_child_selector).all()[:5]
+
+        r_movie = []
+        for m in movies:
+            d = {}
+            name = m.locator("h4").inner_text()
+            d["name"] = name[3:]
+
+            year = m.locator(
+                "xpath=/div/div/div/div[1]/div[2]/div[2]/ul/li[1]"
+            ).inner_text()
+            d["year"] = year
+
+            directors = (
+                m.get_by_text("Director")
+                .locator("xpath=../span[position() > 1]")
+                .all_inner_texts()
+            )
+            d["directors"] = directors
+
+            r_movie.append(d)
+
+        return r_movie
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from chrome_controller import ChromeController
 
-IMDB_WL_URL = r"https://www.imdb.com/user/p.rihuzvwcddwbnucg76npzg62m4/watchlist/?ref_=ext_shr_lnk"
+IMDB_WL_URL = r"https://www.imdb.com/user/p.rihuzvwcddwbnucg76npzg62m4/watchlist/"
 
 # BASE_DIR = Path(__name__).as_posix()
 CHROME_PATH = Path(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
@@ -12,16 +12,6 @@ UL_XPATH = (
     r'//*[@id="__next"]/main/div/section/div/section/div/div[1]/section/div[2]/ul'
 )
 chrome.main_page.goto(url=IMDB_WL_URL)
-
+movies = chrome.get_movies_list(UL_XPATH)
+print(movies)
 input("Press anything to close.")
-
-
-#     ul_locator = page.locator(UL_XPATH)
-#     ul_locator.wait_for()
-
-#     all_movies_list = ul_locator.locator("> li").all()
-
-#     first_5_movies = all_movies_list[:5]
-
-#     print(len(first_5_movies))
-
