d21c36f846536bb0d8ade8715bc4bb668a2491e0
erfaw <erfawn.h@gmail.com>
2026-07-01 15:55:38 +0330

    feat: achieve to film site and search for first movie
    -declare URLs dict and store from donyaye serial archives.

--- main.py
+++ main.py
@@ -15,5 +15,19 @@ UL_XPATH = (
 chrome.main_page.goto(url=IMDB_WL_URL)
 movies = chrome.get_movies_list(UL_XPATH)
 
-print(movies)
+URLs = {
+    "donyaye_serial": {
+        "one_page_archive": "https://dls2.aparatchi-dlcenter.top/DonyayeSerial/donyaye_serial_all_archive.html",
+        "dynamic_archive": "https://dls6.aparatchi-dlcenter.top/DonyayeSerial/10_thous.html", 
+    },
+}
+
+
+chrome.main_page.goto(
+    URLs['donyaye_serial']['dynamic_archive']
+)
+SEARCH_INPUT_XPATH = r'xpath=/html/body/div/div[2]/input'
+search_input_locator = chrome.main_page.locator("xpath=/html/body/div/div[2]/input")
+search_input_locator.fill(f"{movies[0]["name"]} {movies[0]["year"]}")
+search_input_locator.press("Enter")
 input("Press anything to close.")
