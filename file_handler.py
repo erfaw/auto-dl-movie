ffa61cf85d5ee94772e0de048ce233f6c49fd7a3
erfaw <erfawn.h@gmail.com>
2026-07-02 03:11:11 +0330

    docs: update TODO

--- file_handler.py
+++ file_handler.py
@@ -1,1 +1,2 @@
-# TODO : writing binary downloaded movie with continue option here with a class. 
+# TODO : make a FileHandler class to work with files. it must be able to use `shutil` to copy files from a `src` to a `dest` with progressbar using `tqdm`. which for now be called after download procedure. 
+# TODO : after implementing above todo, with `threading parallelism` make io file copying a thread, io file downloading a thread and these 2 threads must talking to each other through a Queue with main thread. 
