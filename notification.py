ddea91ffbb4db4c783e0133808e738e631c3eaae
erfaw <erfawn.h@gmail.com>
2026-08-05 00:21:06 +0330

    feat: implement notification function \w plyer.notification

--- notification.py
+++ notification.py
@@ -1,1 +1,22 @@
-# TODO (Low) : make a notification procedure with a better way than plyer! to be usable for PTK for pyinstaller output too. 
+from plyer import notification
+
+
+def send_plyer(title: str, message: str, timeout :int=5):
+    """
+    Send a notification to the user. using `plyer`_.notification.
+
+    Args:
+        title(str):
+        message(str): message to send with notification.
+        timeout(int):
+
+    Returns:
+        None
+
+    .. _plyer: https://pypi.org/project/plyer/
+    """
+    notification.notify(
+        title=title,
+        message=message,
+        timeout=timeout,
+    ) # type: ignore
